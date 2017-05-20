c28ec9c224b02a812e8bdbd9c48867dd12f35531
Nickmistretta9 <Nick@Nicks-MBP.home>
2017-05-20 16:16:32 -0400

    Added projects view

--- server.js
+++ server.js
@@ -47,6 +47,13 @@ app.get('/about', (req, res) => {
 	});
 });
 
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		pageTitle: 'Project Page',
+		welcomeMessage: 'Welcome to the projects page take a look through my recent work'
+	});
+});
+
 app.get('/bad', (req, res) => {
 	res.send({
 		errorMessage: 'Error bad request'
